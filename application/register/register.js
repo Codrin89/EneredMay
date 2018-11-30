b98b9e840bd20bb7e5e9e7c23ecccbdf6154dd02
BogdanCost <bogdanc85@yahoo.com>
2018-11-30 17:59:05 +0200

    register task finished

--- application/register/register.js
+++ application/register/register.js
@@ -1,35 +1,36 @@
-document.getElementById("register").addEventListener('click', function() {
+document.getElementById("register").addEventListener('click', function () {
+    //validate fields
+    if (!document.getElementById('password').value === document.getElementById('password2').value) {}
 
-	//validate fields
-	if(!document.getElementById('password').value === document.getElementById('password2').value) {
-		// set error fields to password fields
-	}
-	if(document.getElementById('username').value.trim().length === 0) {
-		//set error to username
-		document.getElementById('username').parentElement.classList += ' has-error';
-	}
-	if(document.getElementById('email').value.trim().length === 0) {
-		//set error to email
-	}
-	if(document.getElementById('password').value.trim().length === 0) {
-		//set error to password
-	}
-	if(document.getElementById('password2').value.trim().length === 0) {
-		//set error to password2
-	}
-	var data = {
-		"username": document.getElementById('username').value,
-		"email": document.getElementById('email').value,
-		"password": document.getElementById('password').value,
-	}
 
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if(xhr.status === 200 && xhr.readyState === 4) {
-			console.log(xhr.responseText);
-		}
-	}
+    if (document.getElementById('username').value.trim().length === 0) {
+        //set error to username
+        document.getElementById('username').parentElement.classList += ' has-error';
+    }
+    if (document.getElementById('email').value.trim().length === 0) {
+        //set error to email
+    }
+    if (document.getElementById('password').value.trim().length === 0) {
+        //set error to password
+    }
+    if (document.getElementById('password2').value.trim().length === 0) {
+        //set error to password2
+    }
+    var data = {
+        "username": document.getElementById('username').value,
+        "email": document.getElementById('email').value,
+        "password": document.getElementById('password').value
+    }
 
-	// xhr.open("POST", 'http://localhost/github_enered/EneredMay/api/register', true);
-	// xhr.send(JSON.stringify(data));
+    console.log(data);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.open("POST", 'http://localhost/apii/register', true);
+    xhr.send(JSON.stringify(data));
 });
---
+++ applicationO/register/js/register.js
@@ -0,0 +1,9 @@
+document.getElementById("register").addEventListener('click', function () {
+
+    //validate fields
+    if (document.getElementById('password').value.length.trim() === 0) {
+        //set error to email
+    }
+
+
+})
