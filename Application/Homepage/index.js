1473d4fe45c64d71b52de8424e1f49f1462d3af6
zvanca-alexandru <zvanca_alexandru@yahoo.com>
2018-12-22 15:52:29 +0200

    homepage work on products

--- Application/Homepage/index.js
+++ Application/Homepage/index.js
@@ -32,6 +32,31 @@ $("#mycart").click(function() {
 	}
 });
 
-function createProductsCard() {
-	
+function createProductsCard(data) {
+	data = Object.values(data);
+	var currency = window.localStorage.getItem('currency') || 'RON';
+	for(var i = 0 ; i <  data.length ; i++) {
+		var inStock, inStockClass;
+		if(data[i].stock > 0 ) {
+			inStock = 'In Stock';
+			inStockClass = 'in-stock';
+		} else {
+			inStock = 'Out of Stock';
+			inStockClass = 'out-stock';
+		}
+		var discount;
+		if(data[i].discount > 0 ) {
+			discount = '';
+		} else {
+			discount = 'invisible';
+		}
+		//var inStock = data[i].stock > 0 ? 'In Stock' : 'Out of Stock';//
+		//var discount = data[i].discount > 0 ? '' : 'invisible';
+		var product = $('<div class="product col-xs-12 col-sm-6 col-md-4"><div><h4>'+data[i].name+'</h4><img class="img-responsive" src="'+data[i].image+'"/><p class="'+inStockClass+'">'+inStock+'</p><p class="price">Price: <span class="price">'+data[i].price+ ' ' + currency + '</span></p>'+'<p class="'+discount+'">Discounted price: <span>' + ((100 - data[i].discount)/100 * data[i].price) + ' ' + currency + '</span></p>'+'<button class="btn btn-primary">Vezi detalii</button></div></div>');
+		$('.products-container').append(product);
+	}
 }
+
+$('.currency li a').click(function() {
+	window.localStorage.setItem('currency', this.innerHTML);
+});
